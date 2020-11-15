442e19544e9eca977b4541a70642eb7e46002209
MugiChu <mugichu29@gmail.com>
2020-11-15 17:31:38 +0700

    update ship

--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0, 0, 6)
-        self.ship_sped_factor = 1.5
+        self.ship_speed_factor = 1.5
--- ship.py
+++ ship.py
@@ -7,14 +7,14 @@ class Ship():
         self.screen = screen
         self.ai_settings = ai_settings
 
-        self.center = float(self.rect.centerx)
-
         self.image = pg.image.load('imgs/alienblaster.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
 
         self.moving_right = False
         self.moving_left = False
